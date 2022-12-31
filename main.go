0e2ced928daf93befb3204215e106720a96bd629
952355188@qq.com <952355188@qq.com>
2022-12-31 14:34:04 +0100

    update

--- main.go
+++ main.go
@@ -17,16 +17,21 @@ import (
 )
 
 type msg struct {
-	id      int32
+	// id of the client that sent the message
+	id int32
+	// lamport timestamp of the message
 	lamport uint64
 }
 
-type peer struct {
+type peerNode struct {
 	RA.UnimplementedRAServer
-	id      int32
+	id int32
+	// mutex to protect the state of the peer
 	mutex   sync.Mutex
 	replies uint8
-	held    chan bool
+	// channel to signal that we have entered the critical section
+	held chan bool
+	// state of the peer, 0 = idle, 1 = waiting, 2 = in critical section
 	state   uint8
 	lamport uint64
 	empty   RA.Empty
@@ -39,13 +44,15 @@ type peer struct {
 
 func main() {
 	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32)
-	ownPort := int32(arg1) + 8000
+	nodePort := int32(arg1) + 8000
+	log.Printf("Starting node on port %v", nodePort)
 
+	// create a new peer and start it up on port nodePort (8000 + arg1)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	p := &peer{
-		id:      ownPort,
+	node := &peerNode{
+		id:      nodePort,
 		clients: make(map[int32]RA.RAClient),
 		replies: 0,
 		held:    make(chan bool),
@@ -53,32 +60,34 @@ func main() {
 		state:   0,
 		lamport: 1,
 		empty:   RA.Empty{},
-		idmsg:   RA.Id{Id: ownPort},
+		idmsg:   RA.Id{Id: nodePort},
 		reply:   make(chan bool),
 	}
 
-	// Create listener tcp on port ownPort
-	list, err := net.Listen("tcp", fmt.Sprintf(":%v", ownPort))
+	// Create listener tcp on port nodePort
+	list, err := net.Listen("tcp", fmt.Sprintf(":%v", nodePort))
 	if err != nil {
-		log.Fatalf("Could not listen to port %v: %v", ownPort, err)
+		log.Fatalf("Could not listen to port %v: %v", nodePort, err)
 	}
 
-	f := setLog(ownPort)
-	defer f.Close()
+	file := Log(nodePort)
+	defer file.Close()
 
 	grpcServer := grpc.NewServer()
-	RA.RegisterRAServer(grpcServer, p)
+	RA.RegisterRAServer(grpcServer, node)
 
+	// start the gRPC server
 	go func() {
 		if err := grpcServer.Serve(list); err != nil {
-			log.Fatalf("Failed to serve gRPC server over port %v: %v", ownPort, err)
+			log.Fatalf("Failed to serve gRPC server over port %v: %v", nodePort, err)
 		}
 	}()
 
+	// connect to all other nodes
 	for i := 0; i < 3; i++ {
 		port := 8000 + int32(i)
 
-		if port == ownPort {
+		if port == nodePort {
 			continue
 		}
 
@@ -89,128 +98,120 @@ func main() {
 			log.Fatalf("Could not dial to port %v: %v", port, err)
 		}
 		defer conn.Close()
-		c := RA.NewRAClient(conn)
-		p.clients[port] = c
+		client := RA.NewRAClient(conn)
+		node.clients[port] = client
 	}
 	log.Printf("Dialing done\n")
 	// sleep for 5 seconds to make sure that all clients have been connected
 	time.Sleep(5 * time.Second)
 
-	// start our queue loop, it will (when told to) - send messages that have been delayed
+	// start the main loop
+	// this loop will wait for a reply from a node and then reply to all nodes in the queue
 	go func() {
 		for {
 			// wait for update
-			<-p.reply
-			p.mutex.Lock()
-			for _, msg := range p.queue {
+			<-node.reply
+			node.mutex.Lock()
+			for _, msg := range node.queue {
 				// update our lamport to the max found value in queue
-				if msg.lamport > p.lamport {
-					p.lamport = msg.lamport
+				if msg.lamport > node.lamport {
+					node.lamport = msg.lamport
 				}
 				log.Printf("Replying to %v\n", msg.id)
-				p.clients[msg.id].Reply(p.ctx, &p.idmsg)
+				node.clients[msg.id].Reply(node.ctx, &node.idmsg)
 			}
 			// see previous comment, increment highest found
-			p.lamport++
-			p.queue = nil
-			p.state = 0
-			p.mutex.Unlock()
+			node.lamport++
+			node.queue = nil
+			node.state = 0
+			node.mutex.Unlock()
 		}
 	}()
-
-	rand.Seed(time.Now().UnixNano() / int64(ownPort))
+	// this loop will send a request to a random node every 100ms
+	rand.Seed(time.Now().UnixNano() / int64(nodePort))
 	for {
 		if rand.Intn(100) == 42 {
-			p.mutex.Lock()
-			p.state = 1
-			p.mutex.Unlock()
-			p.enter()
-			<-p.held
+			node.mutex.Lock()
+			node.state = 1
+			node.mutex.Unlock()
+			node.Enter()
+			<-node.held
 			log.Printf("Entered critical section\n")
 			// sleep for 5 seconds in critical section
 			time.Sleep(5 * time.Second)
 			log.Printf("Leaving critical section\n")
-			p.reply <- true
+			node.reply <- true
 		}
 		time.Sleep(100 * time.Millisecond)
 	}
 }
 
-func (p *peer) Request(ctx context.Context, req *RA.Info) (*RA.Empty, error) {
-	p.mutex.Lock()
-	if p.state == 2 || (p.state == 1 && p.LessThan(req.Id, req.Lamport)) {
-		log.Printf("Request | %v is waiting\n", req.Id)
-		p.queue = append(p.queue, msg{id: req.Id, lamport: req.Lamport})
+// request is called by a client to request access to the critical section
+// the client will wait until it is allowed to enter the critical section
+// and then send a reply to all nodes in the queue
+func (node *peerNode) Request(ctx context.Context, req *RA.Info) (*RA.Empty, error) {
+	node.mutex.Lock()
+	// check if we are allowed to enter the critical section
+	if node.state == 2 || (node.state == 1 && node.lamport < req.Lamport) || (node.state == 1 && node.lamport == req.Lamport && node.id < req.Id) {
+		log.Printf("Request- %v is waiting\n", req.Id)
+		node.queue = append(node.queue, msg{id: req.Id, lamport: req.Lamport})
 	} else {
-		if req.Lamport > p.lamport {
-			p.lamport = req.Lamport
+		if req.Lamport > node.lamport {
+			node.lamport = req.Lamport
 		}
-		p.lamport++
+		node.lamport++
 		go func() {
 			// sleep a little, to make sure that the other clients have time to add their messages to the queue
 			time.Sleep(5 * time.Millisecond)
-			log.Printf("Request | %v is allowed\n", req.Id)
-			p.clients[req.Id].Reply(p.ctx, &p.idmsg)
+			log.Printf("Request- %v is allowed\n", req.Id)
+			node.clients[req.Id].Reply(node.ctx, &node.idmsg)
 		}()
 	}
-	p.mutex.Unlock()
-	return &p.empty, nil
+	node.mutex.Unlock()
+	return &node.empty, nil
 }
 
-func (p *peer) LessThan(id int32, lamport uint64) bool {
-	if p.lamport < lamport {
-		return true
-	} else if p.lamport > lamport {
-		return false
-	}
-	if p.id < id {
-		return true
-	}
-	return false
-}
-
-func (p *peer) Reply(ctx context.Context, req *RA.Id) (*RA.Empty, error) {
-	log.Printf("Reply | %v replied\n", req.Id)
-	p.mutex.Lock()
-	p.replies++
-	if p.replies >= 3-1 {
-		p.state = 2
-		p.replies = 0
-		p.mutex.Unlock()
-		p.held <- true
+// reply is called by a client to reply to a request
+func (node *peerNode) Reply(ctx context.Context, req *RA.Id) (*RA.Empty, error) {
+	log.Printf("Reply- %v replied\n", req.Id)
+	node.mutex.Lock()
+	node.replies++
+	if node.replies >= 3-1 {
+		node.state = 2
+		node.replies = 0
+		node.mutex.Unlock()
+		node.held <- true
 	} else {
-		p.mutex.Unlock()
+		node.mutex.Unlock()
 	}
 
-	return &p.empty, nil
+	return &node.empty, nil
 }
 
-func (p *peer) enter() {
+// enter is called by the main loop to request access to the critical section
+func (node *peerNode) Enter() {
 	log.Printf("Enter | Requesting\n")
-	info := &RA.Info{Id: p.id, Lamport: p.lamport}
-	for id, client := range p.clients {
-		_, err := client.Request(p.ctx, info)
+	info := &RA.Info{Id: node.id, Lamport: node.lamport}
+	for id, client := range node.clients {
+		_, err := client.Request(node.ctx, info)
 		if err != nil {
-			log.Printf(" Enter | Failed to request from %v: %v\n", id, err)
+			log.Printf(" Enter- Failed to request from %v: %v\n", id, err)
 		}
-		log.Printf("Enter | Requested %v\n", id)
+		log.Printf("Enter- Requested %v\n", id)
 	}
 }
 
-func setLog(port int32) *os.File {
-	// Clears the log.txt file when a new server is started
+// Log sets up the log file for the server
+func Log(port int32) *os.File {
 	filename := fmt.Sprintf("log%d.txt", port)
 	if err := os.Truncate(filename, 0); err != nil {
 		log.Printf("Error truncating file: %v", err)
 	}
-
-	// This connects to the log file/changes the output of the log informaiton to the log.txt file.
-	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	myfile, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("Error opening file: %v", err)
 	}
-	// print to both file and console
-	mw := io.MultiWriter(os.Stdout, f)
-	log.SetOutput(mw)
-	return f
+	write := io.MultiWriter(os.Stdout, myfile)
+	log.SetOutput(write)
+	return myfile
 }
